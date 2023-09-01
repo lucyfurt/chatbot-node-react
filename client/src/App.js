e1616d576e46fb280d0870efc1696cf78ce6c753
lucyfurt <lucianafurtado10@gmail.com>
2023-09-01 10:21:47 -0300

    clean project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import ChatbotComponent from './Chatbot';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Chatbot </h1>
+        <h2>Apple Store </h2>
       </header>
       <ChatbotComponent />
     </div>
--- client/src/Chatbot.js
+++ client/src/Chatbot.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './chatbot.css';
 
 function App() {
   const [messages, setMessages] = useState([]);
